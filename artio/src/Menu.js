fd70b811222e86a82d54fc9defc84c91006d6654
ariel <santa.cruz.unla@gmail.com>
2023-01-05 20:11:16 -0300

    primer componente anidado, Menu dentro de App. estatico, solo render

---
+++ artio/src/Menu.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Menu extends React.Component{
+
+    render(){
+        return(
+            <div calss="row" >
+                <div class="col-12 d-flex align-items-center">
+                    
+                    <h1>Artio</h1>
+                    <span class="mx-3" >inicio</span>
+                    <span class="mx-3" >artista</span>
+                    <span class="mx-3" >contacto</span>
+                </div>
+            </div>
+        )
+    }
+} 
