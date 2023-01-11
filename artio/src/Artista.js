2ee8ca0f37b22d44ee93ad99bb03553bb2e44137
ariel <santa.cruz.unla@gmail.com>
2023-01-10 22:05:48 -0300

    se agrego un btn con un state que oculte o muestre la informacion

--- artio/src/Artista.js
+++ artio/src/Artista.js
@@ -1,22 +1,44 @@
 import React from "react";
 import './Artista.css';
 
+
+ 
+
+
 export default class Artista extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            escondido:false
+        }
+
+        this.toggleEsconder=this.toggleEsconder.bind(this);
     }
+
+    toggleEsconder(){
+        this.setState( prevState => ( {
+            escondido: !prevState.escondido
+        } ))
+    }
+
+
+
+
+
     render(){
         return(
             <div className="row">
+                
                 <div className="col-12 d-flex align-items-center pt-2">
-                    <img  src={this.props.img} alt={this.props.nombre} className="band-thumb"/>
+                    <img  src={this.props.img} alt={this.props.nombre} className={(this.state.escondido? "d-none":"band-thumb")}/>
                     <div className="px-2">
-                        <h2>{this.props.nombre}</h2>
-                        <p> {this.props.descripcion}</p>
+                        <h2 className={ (this.state.escondido? "d-none":"") }>{this.props.nombre}</h2>
+                        <p  className={ (this.state.escondido? "d-none":"") }> {this.props.descripcion}</p>
+                        <button type="button" onClick={this.toggleEsconder} class="btn btn-primary btn1" >{ (this.state.escondido? "abrir":"cerrar")+" "+(this.state.escondido? this.props.nombre:"") } </button>                            
                     </div>
-                    
-
                 </div>
+                
+                
 
             </div>
 
